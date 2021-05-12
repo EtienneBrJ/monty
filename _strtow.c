5f4a4ca24cc094d373163ee03dd03f0f331ccc2c
EtienneBrJ <etiennebrxv@gmail.com>
2021-05-12 17:03:53 +0200

    fix push arguments error

--- _strtow.c
+++ _strtow.c
@@ -20,6 +20,9 @@ char **_strtow(char *prmString, char *prmSeparators, char *prmEscapeSeparators)
 	size = strlen(prmString);
 	word_number = _wordNumber(prmString, prmSeparators);
 
+	if (word_number != 2)
+		_errorHandler(INVALID_ARGUMENT_NUMBER);
+
 	if (prmString == NULL || !prmString || word_number == 0)
 		return (NULL);
 
